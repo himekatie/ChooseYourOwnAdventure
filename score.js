b1e7f197a9b53a58f8631444d34710610e0ba285
himekatie <useforanything9@gmail.com>
2023-11-05 00:55:27 -0400

    commit

---
+++ score.js
@@ -0,0 +1,16 @@
+var score;
+
+function incrementGood(){
+    score = score +3;
+}
+
+function incrementNeutral(){
+    score = score +2;
+}
+
+function incrementbad(){
+    score = score +0;
+}
+function findout(){
+    console.log(score);
+}
